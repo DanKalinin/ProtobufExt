ed44e7439faecdfe046d47416822fddd379738cc
Dan Kalinin <daniil551@hotmail.com>
2019-01-08 00:21:00 +0300

    RPC

--- ProtobufExt/GPBERPC.h
+++ ProtobufExt/GPBERPC.h
@@ -9,130 +9,130 @@
 #import <Helpers/Helpers.h>
 #import "Payload.pbobjc.h"
 
-@class PB3RPCPayloadReading, PB3RPCPayloadWriting, PB3RPC;
-
-
-
-
-
-
-
-
-
-
-@interface PB3RPCPayloadReading : HLPRPCPayloadReading
-
-@property (readonly) HLPStreamReading *lengthReading;
-@property (readonly) HLPStreamReading *payloadReading;
-
-@end
-
-
-
-
-
-
-
-
-
-
-@interface PB3RPCPayloadWriting : HLPRPCPayloadWriting
-
-@property (readonly) HLPStreamWriting *lengthWriting;
-@property (readonly) HLPStreamWriting *payloadWriting;
-
-@end
-
-
-
-
-
-
-
-
-
-
-@interface PB3RPC : HLPRPC
-
-@end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@class GPBERPCReading;
-@class GPBERPCWriting;
-@class GPBERPC;
-
-
-
-
-
-
-
-
-
-
-@protocol GPBERPCReadingDelegate <NSERPCReadingDelegate>
-
-@end
-
-
-
-@interface GPBERPCReading : NSERPCReading <GPBERPCReadingDelegate>
-
-@property (readonly) NSEStreamReading *lengthReading;
-@property (readonly) NSEStreamReading *reading;
-
-@end
-
-
-
-
-
-
-
-
-
-
-@protocol GPBERPCWritingDelegate <NSERPCWritingDelegate>
-
-@end
-
-
-
-@interface GPBERPCWriting : NSERPCWriting <GPBERPCWritingDelegate>
-
-@property (readonly) NSEStreamWriting *lengthWriting;
-@property (readonly) NSEStreamWriting *writing;
-
-@end
-
-
-
-
-
-
-
-
-
-
-@protocol GPBERPCDelegate <NSERPCDelegate>
-
-@end
-
-
-
-@interface GPBERPC : NSERPC <GPBERPCDelegate>
-
-@end
+//@class PB3RPCPayloadReading, PB3RPCPayloadWriting, PB3RPC;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@interface PB3RPCPayloadReading : HLPRPCPayloadReading
+//
+//@property (readonly) HLPStreamReading *lengthReading;
+//@property (readonly) HLPStreamReading *payloadReading;
+//
+//@end
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@interface PB3RPCPayloadWriting : HLPRPCPayloadWriting
+//
+//@property (readonly) HLPStreamWriting *lengthWriting;
+//@property (readonly) HLPStreamWriting *payloadWriting;
+//
+//@end
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@interface PB3RPC : HLPRPC
+//
+//@end
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@class GPBERPCReading;
+//@class GPBERPCWriting;
+//@class GPBERPC;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@protocol GPBERPCReadingDelegate <NSERPCReadingDelegate>
+//
+//@end
+//
+//
+//
+//@interface GPBERPCReading : NSERPCReading <GPBERPCReadingDelegate>
+//
+//@property (readonly) NSEStreamReading *lengthReading;
+//@property (readonly) NSEStreamReading *reading;
+//
+//@end
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@protocol GPBERPCWritingDelegate <NSERPCWritingDelegate>
+//
+//@end
+//
+//
+//
+//@interface GPBERPCWriting : NSERPCWriting <GPBERPCWritingDelegate>
+//
+//@property (readonly) NSEStreamWriting *lengthWriting;
+//@property (readonly) NSEStreamWriting *writing;
+//
+//@end
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//@protocol GPBERPCDelegate <NSERPCDelegate>
+//
+//@end
+//
+//
+//
+//@interface GPBERPC : NSERPC <GPBERPCDelegate>
+//
+//@end
